e3b62b6935ab7aa141594b70c973ddb94da4223c
Damian Bukowski <damianbukowski@email.com>
2020-05-16 10:26:35 +0200

    Singleton Enum

--- src/main/java/com/sda/Singleton/QueueSingletonEnum.java
+++ src/main/java/com/sda/Singleton/QueueSingletonEnum.java
@@ -3,31 +3,19 @@ package com.sda.Singleton;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Queue {
-    private static Queue instance = new Queue();
+public enum QueueSingletonEnum {
+    INSTANCE;
 
-    public static Queue instanceOf() {
-        if (instance == null) {
-            instance = new Queue();
-        }
-        return instance;
-
-    }
     private List<String> names;
 
-    private Queue() {
+    QueueSingletonEnum() {
         this.names = new LinkedList<>();
-
-        //ustawianie javy na JAVE 8 wpisujemy to na dole a potem alt+enter i sent language on 8
-        //() -> { }
     }
 
-    //dodawanie element do kolejki
     public void push(String name) {
         this.names.add(name);
-
     }
-    //zdejmuje kolejny element z kolejki
+
     public String pop() {
         if (this.names.isEmpty()) {
             throw new RuntimeException("Queue is empty");
@@ -40,3 +28,4 @@ public class Queue {
 
     }
 }
+
--- src/main/java/com/sda/Singleton/SingletonApplication.java
+++ src/main/java/com/sda/Singleton/SingletonApplication.java
@@ -7,6 +7,7 @@ public class SingletonApplication {
       Queue queue3 = Queue.instanceOf();
       Queue queue = Queue.instanceOf();
 
+      QueueSingletonEnum instance = QueueSingletonEnum.INSTANCE;
       queue.push("Szymon");
       queue.push("Jan");
       queue.push("Damian");
