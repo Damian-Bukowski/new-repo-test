446237e4c1f3c61dd3496dc154275b11ec08ceae
Damian Bukowski <damianbukowski@email.com>
2020-05-16 10:19:50 +0200

    Instance lazy singleton

--- src/main/java/com/sda/Singleton/SingletonApplication.java
+++ src/main/java/com/sda/Singleton/SingletonApplication.java
@@ -2,7 +2,10 @@ package com.sda.Singleton;
 
 public class SingletonApplication {
     public static void main(String[] args) {
-      Queue queue = new Queue();
+      Queue queue1 = Queue.instanceOf();
+      Queue queue2 = Queue.instanceOf();
+      Queue queue3 = Queue.instanceOf();
+      Queue queue = Queue.instanceOf();
 
       queue.push("Szymon");
       queue.push("Jan");
