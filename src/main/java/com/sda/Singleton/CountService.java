a5dcbc2ebb43ecdb79f20a7fad4aad7d5e015c78
Damian Bukowski <damianbukowski@email.com>
2020-05-16 10:54:39 +0200

    Singleton Enum

---
+++ src/main/java/com/sda/Singleton/CountService.java
@@ -0,0 +1,26 @@
+package com.sda.Singleton;
+
+public class CountService {
+
+    private static CountService instance;
+
+    public static CountService instanceOf() {
+        if (instance == null) {
+            instance = new CountService();
+            }
+            return instance;
+    }
+
+    private Integer count = 0;
+
+private CountService() {}
+
+public void increment() {
+    this.count = this.count + 1;
+}
+
+public Integer getCount() {
+    return count;
+
+}
+}
