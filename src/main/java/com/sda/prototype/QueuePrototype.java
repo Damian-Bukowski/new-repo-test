b1ec7f984a8e404349ad81feb16733c66cd305ff
Damian Bukowski <damianbukowski@email.com>
2020-05-16 12:47:45 +0200

    Prototype #3

--- src/main/java/com/sda/prototype/QueuePrototype.java
+++ src/main/java/com/sda/prototype/QueuePrototype.java
@@ -6,30 +6,35 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class QueuePrototype {
-    private static List<QueuePrototype> instances = new ArrayList<QueuePrototype>()
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-    {{
-        this.add(null);
-        this.add(null);
-        this.add(null);
+public class QueuePrototype {
+    private static List<QueuePrototype> instances = new ArrayList<QueuePrototype>() {{
+        this.add(new QueuePrototype());
+        this.add(new QueuePrototype());
+        this.add(new QueuePrototype());
     }};
+
     public static QueuePrototype instanceOf(int index) {
         if (index > 2 || index < 0) {
             return null;
         }
 
-        QueuePrototype queuePrototype = instances.get(index);
-        if (queuePrototype == null) {
-            QueuePrototype element = new QueuePrototype();
-            instances.add(index, element);
-            return element;
-        }
-        return queuePrototype;
+        return instances.get(index);
     }
+
     private List<String> names;
-    private QueuePrototype() {this.names = new LinkedList<>(); }
-    public void push(String name) { this.names.add(name); }
+
+    private QueuePrototype() {
+        this.names = new LinkedList<>();
+    }
+
+    public void push(String name) {
+        this.names.add(name);
+    }
+
     public String pop() {
         if (this.names.isEmpty()) {
             throw new RuntimeException("Queue is empty");
@@ -37,7 +42,5 @@ public class QueuePrototype {
         String value = this.names.get(0);
         this.names.remove(0);
         return value;
-
-
     }
 }
