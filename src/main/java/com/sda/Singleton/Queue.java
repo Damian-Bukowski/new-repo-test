13555712815d726a6e85c645e9ed57825f506978
Damian Bukowski <damianbukowski@email.com>
2020-05-16 10:09:03 +0200

    dada

---
+++ src/main/java/com/sda/Singleton/Queue.java
@@ -0,0 +1,33 @@
+package com.sda.Singleton;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Queue {
+    private List<String> names;
+
+    public Queue() {
+        this.names = new LinkedList<>();
+
+        //ustawianie javy na JAVE 8 wpisujemy to na dole a potem alt+enter i sent language on 8
+        //() -> { }
+    }
+
+    //dodawanie element do kolejki
+    public void push(String name) {
+        this.names.add(name);
+
+    }
+    //zdejmuje kolejny element z kolejki
+    public String pop() {
+        if (this.names.isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+
+        }
+        String value = this.names.get(0);
+        this.names.remove(0);
+        return value;
+
+
+    }
+}
---
+++ src/main/java/com/sda/Singleton/SingletonApplication.java
@@ -0,0 +1,17 @@
+package com.sda.Singleton;
+
+public class SingletonApplication {
+    public static void main(String[] args) {
+      Queue queue = new Queue();
+
+      queue.push("Szymon");
+      queue.push("Jan");
+      queue.push("Damian");
+
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+
+        System.out.println(queue.pop());
+    }
+}
